0f705e05ec52eb89e5642bdb565b69252c9eb7f2
Haruki Okuda <haruki.for.01@gmail.com>
2021-01-31 17:27:24 +0900

    h1のスタイルをscopedで適用
    Signed-off-by: Haruki Okuda <haruki.for.01@gmail.com>

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -34,5 +34,53 @@
 
 </script>
 <style lang="scss" scoped>
+ .top {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: calc(100vh - 80px);
+    overflow: hidden;
+    margin: 0px;
+    padding: 80px 0 0 0;
 
+    @include sp {
+      justify-content: center;
+      padding: 0;
+      height: 100vh;
+    }
+
+    .typoWrap {
+      padding-left: 100px;
+
+      @include sp {
+        padding-left: 0px;
+      }
+    }
+  }
+  h1 {
+    font-size: 64px;
+    font-weight: bold;
+    color: $mc02;
+    margin-bottom: 15px;
+
+    @include sp {
+      font-size: 40px;
+    }
+
+    &+p {
+      font-size: 32px;
+      color: $mc04;
+      transition: opacity 1s;
+      opacity: 0;
+
+      @include sp {
+        text-align: center;
+        font-size: 20px;
+      }
+
+      span {
+        color: $mc03;
+      }
+    }
+  }
 </style>
