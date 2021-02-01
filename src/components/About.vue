19088455a712c4143fb8b8ba959f090a5afa304a
Haruki Okuda <haruki.for.01@gmail.com>
2021-02-01 22:53:06 +0900

    fix 見出しをミニマルに
    Signed-off-by: Haruki Okuda <haruki.for.01@gmail.com>

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,21 +1,13 @@
 <template>
   <section id="about" class="about">
-    <div class="inner">
-      <h1>Skills</h1>
+      <h1>About</h1>
       <p>WEB制作に必要なHTML5, CSS3, JavaScriptをはじめ、<br>
         アプリ開発のためのVue.jsも書きます。</p>
-    </div>
-    <div class="inner">
-      <h1>Blog<span>ブログ</span></h1>
       <p>気になった技術をまとめたり、<br>
         プログラミングスクールで教えている生徒に向けて書いた記事が中心です。</p>
       <a href="" class="btn">ボタン</a>
-    </div>
-    <div class="inner">
-      <h1>Profile<span>人物紹介</span></h1>
       <p>私のライフヒストリーをかんたんにまとめてみました。<br>
         ミニマリストでなんでも効率化しがちです。</p>
-    </div>
   </section>
 </template>
 <script>
