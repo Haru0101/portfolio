3b6f5ac813fe927f2eba570b59aca28aef70f749
Haruki Okuda <haruki.for.01@gmail.com>
2021-02-03 22:07:36 +0900

    背景画像をローカルで読み込み
    Signed-off-by: Haruki Okuda <haruki.for.01@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@
 }
 
 #particles-js {
-    background-image: url("https://images.unsplash.com/photo-1554147090-e1221a04a025?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=731&q=80");
+    background-image: url("./assets/img/bg.jpg");
     background-size: cover;
     position: absolute;
     top: 0;
