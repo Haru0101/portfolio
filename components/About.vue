2fcbaef1a7a37227160631bc2126ae00d51cbe8c
Haru0101 <haruki.for.01@gmail.com>
2021-11-03 17:31:33 +0900

    ざっくりソースを移植
    Signed-off-by: Haru0101 <haruki.for.01@gmail.com>

---
+++ components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <main id="about" class="about">
+      <h1>ABOUT</h1>
+      <p>WEB制作に必要なHTML5, CSS3, JavaScriptをはじめ、<br>
+        アプリ開発のためのVue.jsも書きます。</p>
+  </main>
+</template>
+<script>
+  export default {
+
+  }
+
+</script>
+<!-- <style lang="scss" scoped>
+
+</style> -->
---
+++ components/Works.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+      <h1>WORKS</h1>
+      <!-- <p>趣味で作ったPWAやサイトなどです。<br>
+                    使用言語はJavaScriptと、そのフレームワークが中心です。</p> -->
+      <p>準備中</p>
+  </main>
+</template>
