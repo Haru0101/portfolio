fb3b60695a7e288c83f71ab98adac1bdb6cceca4
Haru0101 <haruki.for.01@gmail.com>
2021-11-04 16:55:40 +0900

    共通Scss移植

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,31 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
+
+<style lang="scss">
+// カラー変数
+$mc01: #e9ebf2;
+$mc02: #d2dae6;
+$mc03: #1f2e40;
+$mc04: #526a81;
+
+$sp: 768px; // スマホ
+$pc: 769px; // PC
+
+@mixin pc {
+    @media (min-width: ($pc)) {
+        @content;
+    }
+}
+
+@mixin sp {
+    @media (max-width: ($sp)) {
+        @content;
+    }
+}
+
+body {
+    box-sizing: border-box;
+}
+
+</style>
