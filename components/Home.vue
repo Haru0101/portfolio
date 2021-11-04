989533df4e1b4bf0359ca4b0df20a79715b63c1c
Haru0101 <haruki.for.01@gmail.com>
2021-11-04 16:22:57 +0900

    CSSの冗長性を排除
    Signed-off-by: Haru0101 <haruki.for.01@gmail.com>

--- components/Home.vue
+++ components/Home.vue
@@ -33,52 +33,50 @@
   }
 
 </script>
-<!-- <style lang="scss" scoped> 
-//   .top {
-//     padding-left: 100px;
-//     display: flex;
-//     align-items: center;
-//     font-family: "游明朝", YuMincho, "Hiragino Mincho ProN W3", "ヒラギノ明朝 ProN W3", "Hiragino Mincho ProN", "HG明朝E", "ＭＳ Ｐ明朝", "ＭＳ 明朝", serif;
+<style lang="scss" scoped> 
+  .top {
+    padding-left: 100px;
+    display: flex;
+    align-items: center;
+    font-family: "游明朝", YuMincho, "Hiragino Mincho ProN W3", "ヒラギノ明朝 ProN W3", "Hiragino Mincho ProN", "HG明朝E", "ＭＳ Ｐ明朝", "ＭＳ 明朝", serif;
 
-//     @include sp {
-//       width: 100%;
-//       padding-left: 0;
-//       flex-direction: column;
-//       justify-content: center;
-//       padding: 0;
-//     }
+    @media (max-width: 834px) {
+      width: 100%;
+      padding: 0;
+      flex-direction: column;
+      justify-content: center;
+    }
 
-//     h1 {
-//       position: static;
-//       font-size: 64px;
-//       font-weight: bold;
-//       color: $mc03;
-//       margin-bottom: 15px;
-//       margin-bottom: 0;
-//       letter-spacing: 0.02em;
+    h1 {
+      position: static;
+      font-size: 64px;
+      font-weight: bold;
+      color: $mc03;
+      margin-bottom: 15px;
+      margin-bottom: 0;
+      letter-spacing: 0.02em;
 
-//       @include sp {
-//         font-size: 40px;
-//         text-align: center;
-//       }
+      @media (max-width: 834px) {
+        font-size: 40px;
+        text-align: center;
+      }
 
-//       &+p {
-//         font-size: 32px;
-//         transition: opacity 1s;
-//         opacity: 0;
+      &+p {
+        font-size: 32px;
+        transition: opacity 1s;
+        opacity: 0;
 
-//         @include sp {
-//           text-align: center;
-//           font-size: 20px;
-//           padding-right: 5px;
-//         }
+        @media (max-width: 834px) {
+          text-align: center;
+          font-size: 20px;
+          padding-right: 5px;
+        }
 
-//         span {
-//           color: $mc04;
-//         }
-//       }
-//     }
-//   }
+        span {
+          color: $mc04;
+        }
+      }
+    }
+  }
 
 </style>
--->
