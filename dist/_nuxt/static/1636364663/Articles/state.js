06580f8e8f85510ece49e36db542c2f540216fd1
Haru0101 <haruki.for.01@gmail.com>
2021-11-08 20:46:13 +0900

    vue cliのアップデート
    Signed-off-by: Haru0101 <haruki.for.01@gmail.com>

--- dist/_nuxt/static/1636364663/Articles/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636364663",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
