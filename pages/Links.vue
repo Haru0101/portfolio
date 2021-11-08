d52f454d745ebb580b60833643f9a1801706a295
Haru0101 <haruki.for.01@gmail.com>
2021-11-08 18:30:53 +0900

    ブログのリンク追加
    Signed-off-by: Haru0101 <haruki.for.01@gmail.com>

--- pages/Links.vue
+++ pages/Links.vue
@@ -8,6 +8,7 @@
           @harryduck00</a>
         <a href="https://github.com/Haru0101" class="snsBtn" target="_blank" rel="noopener">GitHub: @Haru0101</a>
         <a href="https://zenn.dev/harryduck" class="snsBtn" target="_blank" rel="noopener">Zenn: @harryduck</a>
+        <a href="https://duckwell.netlify.app/" class="snsBtn" target="_blank" rel="noopener">Blog: DuckWell</a>
       </div>
     </div>
   </main>
