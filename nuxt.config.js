29046f589b78e912aefe6ea7bb041d8bb950fdbb
Haru0101 <haruki.for.01@gmail.com>
2021-11-09 19:32:23 +0900

    titleの変更
    Signed-off-by: Haru0101 <haruki.for.01@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'Haruki Okuda',
     htmlAttrs: {
       lang: 'ja'
     },
