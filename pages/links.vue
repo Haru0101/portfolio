60aeb682d956a47c2c68d2e242f6e99e1afc3c63
Haru0101 <haruki.for.01@gmail.com>
2023-01-14 17:58:35 +0900

    fix: remove unnecessary twitter link

--- pages/links.vue
+++ pages/links.vue
@@ -4,8 +4,6 @@
       <h1>LINKS</h1>
       <p>お気軽にお問い合わせください</p>
       <div class="btnWrap">
-        <a href="https://twitter.com/harryduck00" class="snsBtn" target="_blank" rel="noopener">Twitter:
-          @harryduck00</a>
         <a href="https://github.com/Haru0101" class="snsBtn" target="_blank" rel="noopener">GitHub: @Haru0101</a>
         <a href="https://zenn.dev/harryduck" class="snsBtn" target="_blank" rel="noopener">Zenn: @harryduck</a>
         <a href="https://duckwell.netlify.app/" class="snsBtn" target="_blank" rel="noopener">Blog: DuckWell</a>
